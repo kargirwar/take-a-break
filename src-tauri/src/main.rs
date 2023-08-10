4e5810a9baf8095edf0602f8c4ac0aa54139693c
Pankaj Kargirwar <kargirwar@protonmail.com>
2023-08-10 08:59:27 +0530

    Testing state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,12 +7,19 @@ use std::io::{Cursor};
 use plist::Value;
 use std::fmt;
 use std::thread;
+use tokio::sync::mpsc::{Sender, Receiver};
+use tokio::sync::mpsc;
 
 enum LockedState {
     Locked,
     Unlocked,
     Unknown,
 }
+
+struct MyState(String);
+struct MyString(String);
+
+//struct Comm(Sender<&str>, Receiver<&str>);
 
 impl fmt::Debug for LockedState {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -24,9 +31,26 @@ impl fmt::Debug for LockedState {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx): (Sender<u8>, Receiver<u8>) = mpsc::channel(10);
+
+    tokio::spawn(async move  {
+        loop {
+            match rx.recv().await {
+                Some(i) => println!("{:?}", i),
+                None => println!("None"),
+            }
+        }
+    });
+
+    tauri::async_runtime::set(tokio::runtime::Handle::current());
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(MyState("some state value".into()))
+        .manage(MyString("Hello, managed state!".to_string()))
+        .manage(tx)
+        .invoke_handler(tauri::generate_handler![greet, my_custom_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -72,23 +96,30 @@ fn is_locked() -> LockedState {
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-    let _handle = thread::spawn(|| {
-        println!("{:?}", is_locked());
-        let mut sl = Soloud::default().unwrap();
-        sl.set_global_volume(3.0);
-
-        let mut wav = audio::Wav::default();
-
-        wav
-            .load_mem(include_bytes!("beep.mp3"))
-            .unwrap();
-
-        sl.play(&wav);
-
-        while sl.voice_count() > 0 {
-            std::thread::sleep(std::time::Duration::from_millis(100));
-        }
-    });
+    println!("Greet called");
+    //let _handle = thread::spawn(|| {
+        //println!("{:?}", is_locked());
+        //let mut sl = Soloud::default().unwrap();
+        //sl.set_global_volume(3.0);
+//
+        //let mut wav = audio::Wav::default();
+//
+        //wav
+            //.load_mem(include_bytes!("beep.mp3"))
+            //.unwrap();
+//
+        //sl.play(&wav);
+//
+        //while sl.voice_count() > 0 {
+            //std::thread::sleep(std::time::Duration::from_millis(100));
+        //}
+    //});
 
     format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+async fn my_custom_command(tx: tauri::State<'_, Sender<u8>>) -> Result<(), ()> {
+    tx.send(100).await;
+    Ok(())
 }
