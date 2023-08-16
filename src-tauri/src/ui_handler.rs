921086511402ed92437a46e2c7615e8521d473ed
Pankaj Kargirwar <kargirwar@protonmail.com>
2023-08-16 11:30:41 +0530

    Testing alarms

--- src-tauri/src/ui_handler.rs
+++ src-tauri/src/ui_handler.rs
@@ -5,9 +5,9 @@ mod ui_handler {
     use serde::Deserialize;
     use tauri::AppHandle;
     use tauri::Wry;
+    use tokio::select;
     use tokio::sync::mpsc;
     use tokio::sync::mpsc::{Receiver, Sender};
-    use tokio::select;
 
     pub enum CommandName {
         UpdateRules,
@@ -83,50 +83,21 @@ mod ui_handler {
         }
 
         async fn handle_command(&self, cmd: String) {
-            println!("{:?}", cmd);
-
             if let Ok(json) = serde_json::from_str::<serde_json::Value>(&cmd) {
-                println!("Parsed JSON: {:#?}", json);
-
                 if let Some(name) = json.get("name").and_then(|n| n.as_str()) {
                     match CommandName::from_str(name) {
-                        //Some(CommandName::UpdateRules) => handle_update_rules(json),
                         Some(CommandName::UpdateRules) => {
-                            println!("handle_update_rules");
                             self.am_tx.send(cmd).await.unwrap();
                         }
                         _ => println!("none"),
                     }
                 } else {
-                    println!("No 'name' field or not a string in the JSON object.");
+                    println!("ui_handler: No 'name' field or not a string in the JSON object.");
                 }
             } else {
-                eprintln!("Error while parsing JSON");
+                eprintln!("ui_handler: Error while parsing JSON");
             }
         }
-
-        //
-        //
-
-        //fn handle_rules_updated(json: serde_json::Value) {
-        //if let Some(rules) = json.get("rules").and_then(serde_json::Value::as_array) {
-        //let mut rule_objects: Vec<Rule> = Vec::new();
-        //
-        //for rule_json in rules {
-        //let rule: Rule =
-        //serde_json::from_value(rule_json.clone()).expect("Rule deserialization error");
-        //rule_objects.push(rule);
-        //}
-        //
-        //println!("{:#?}", rule_objects);
-        //update_alarms(rule_objects);
-        //}
-        //}
-        //
-        //fn update_alarms(rules: Vec<Rule>) {
-        //let alarms = get_alarms(&rules);
-        //println!("alarms: {:?}", alarms);
-        //}
     }
 }
 
--- src-tauri/src/ui_handler/alarm_manager.rs
+++ src-tauri/src/ui_handler/alarm_manager.rs
@@ -1,6 +1,7 @@
 mod alarm_manager {
-    //use crate::Rule;
-    //use std::collections::HashMap;
+    use crate::Rule;
+    use crate::CommandName;
+    use std::collections::HashMap;
     use tokio::sync::mpsc::{Receiver, Sender};
 
     pub struct AlarmManager {
@@ -14,86 +15,131 @@ mod alarm_manager {
         }
 
         pub async fn run(mut self) {
-            println!("Running AlarmManager");
+            println!("alarm_manager:Running AlarmManager");
             tokio::spawn(async move {
                 loop {
                     match self.rx.recv().await {
                         Some(i) => {
-                            println!("AlarmManager: {}", i);
-                            self.tx.send("Acknowledged".to_string()).await.unwrap();
+                            self.handle_command(i).await;
                         },
                         None => print!("e"),
                     }
                 }
             });
         }
+
+        async fn handle_command(&self, cmd: String) {
+            println!("alarm_manager:{:?}", cmd);
+
+            if let Ok(json) = serde_json::from_str::<serde_json::Value>(&cmd) {
+                println!("alarm_manager:Parsed JSON: {:#?}", json);
+
+                if let Some(name) = json.get("name").and_then(|n| n.as_str()) {
+                    match CommandName::from_str(name) {
+                        Some(CommandName::UpdateRules) => {
+                            handle_update_rules(json);
+                            self.tx.send("Ack".to_string()).await.unwrap();
+                        }
+                        _ => println!("alarm_manager:none"),
+                    }
+                } else {
+                    println!("alarm_manager:No 'name' field or not a string in the JSON object.");
+                }
+            } else {
+                eprintln!("alarm_manager:Error while parsing JSON");
+            }
+        }
     }
 
-    //fn get_hours(s: usize, e: usize) -> Vec<usize> {
-        //let mut hrs = Vec::new();
-//
-        //let mut current_hour = s;
-        //while current_hour <= e {
-            //hrs.push(current_hour);
-            //current_hour += 1;
-        //}
-//
-        //hrs
-    //}
-//
-    //pub fn get_alarms(rules: &[Rule]) -> HashMap<String, HashMap<usize, Vec<usize>>> {
-        //let mut alarms: HashMap<String, HashMap<usize, Vec<usize>>> = HashMap::new();
-//
-        //for r in rules {
-            //let hours: HashMap<usize, Vec<usize>> = HashMap::new();
-//
-            //for d in &r.days {
-                //alarms.insert(d.clone(), hours.clone());
-//
-                //let s = r.from;
-                //let e = r.to;
-                //let f = r.interval;
-                //let hrs = get_hours(s, e);
-//
-                //let mut i = 0;
-                //let mut m = f;
-                //let mut h;
-//
-                //loop {
-                    //let mut mins = Vec::new();
-//
-                    //loop {
-                        //mins.push(m % 60);
-                        //m += f;
-//
-                        //if m - (60 * i) >= 60 {
-                            //h = hrs[i];
-//
-                            //if f == 60 && h == s {
-                                //i += 1;
-                                //break;
-                            //}
-//
-                            //println!("{} h: {} mins: {:?}", d, h, mins);
-//
-                            //i += 1;
-                            //break;
-                        //}
-                    //}
-//
-                    //if e == hrs[i] {
-                        //if m % 60 == 0 {
-                            //alarms.get_mut(d).unwrap().insert(e, vec![0]);
-                            //println!("{} h: {} mins: {:?}", d, e, alarms[d].get(&e).unwrap());
-                        //}
-                        //break;
-                    //}
-                //}
-            //}
-        //}
-//
-        //alarms
-    //}
+    fn handle_update_rules(json: serde_json::Value) {
+        if let Some(rules) = json.get("rules").and_then(serde_json::Value::as_array) {
+            let mut rule_objects: Vec<Rule> = Vec::new();
+
+            for rule_json in rules {
+                let rule: Rule =
+                    serde_json::from_value(rule_json.clone()).expect("alarm_manager:Rule deserialization error");
+                rule_objects.push(rule);
+            }
+
+            println!("alarm_manager:{:#?}", rule_objects);
+            update_alarms(rule_objects);
+        }
+    }
+
+    fn update_alarms(rules: Vec<Rule>) {
+        let alarms = get_alarms(&rules);
+        println!("alarm_manager:alarms: {:?}", alarms);
+    }
+
+    fn get_hours(s: usize, e: usize) -> Vec<usize> {
+        let mut hrs = Vec::new();
+
+        let mut current_hour = s;
+        while current_hour <= e {
+            hrs.push(current_hour);
+            current_hour += 1;
+        }
+
+        hrs
+    }
+
+    fn get_alarms(rules: &[Rule]) -> HashMap<String, HashMap<usize, Vec<usize>>> {
+        let mut alarms: HashMap<String, HashMap<usize, Vec<usize>>> = HashMap::new();
+
+        for r in rules {
+            let mut hours: HashMap<usize, Vec<usize>> = HashMap::new();
+
+            for d in &r.days {
+                alarms.insert(d.clone(), hours.clone());
+
+                let s = r.from;
+                let e = r.to;
+                let f = r.interval;
+                let hrs = get_hours(s, e);
+
+                let mut i = 0;
+                let mut m = f;
+                let mut h = 0;
+
+                loop {
+                    let mut mins = Vec::new();
+
+                    loop {
+                        mins.push(m % 60);
+                        m += f;
+
+                        if m - (60 * i) >= 60 {
+                            h = hrs[i];
+
+                            if f == 60 && h == s {
+                                i += 1;
+                                break;
+                            }
+
+                            alarms.get_mut(d).unwrap().insert(h, mins.clone());
+                            println!("{} h: {} mins: {:?}", d, h, mins);
+
+                            i += 1;
+                            break;
+                        }
+                    }
+
+                    if e == hrs[i] {
+                        if m % 60 == 0 {
+                            alarms
+                                .get_mut(d)
+                                .unwrap()
+                                .insert(e, vec![0]);
+                            println!("{} h: {} mins: {:?}", d, e, alarms[d].get(&e).unwrap());
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        alarms
+    }
 }
-//
+
 pub use alarm_manager::*;
